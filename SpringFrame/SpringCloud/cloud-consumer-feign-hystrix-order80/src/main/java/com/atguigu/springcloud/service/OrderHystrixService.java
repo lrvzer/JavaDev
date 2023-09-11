c647745edb2f8c8f2b593ff61634bf96c51efeb6
Lrwei <a2413044193@gmail.com>
2023-09-11 14:38:16 +0800

    hystrix-@FeignClient(value = "CLOUD-PROVIDER-HYSTRIX-SERVICE", fallback = OrderHystrixFallbackService.class) 服务宕机问题

--- SpringFrame/SpringCloud/cloud-consumer-feign-hystrix-order80/src/main/java/com/atguigu/springcloud/service/OrderHystrixService.java
+++ SpringFrame/SpringCloud/cloud-consumer-feign-hystrix-order80/src/main/java/com/atguigu/springcloud/service/OrderHystrixService.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 @Component
-@FeignClient(value = "CLOUD-PROVIDER-HYSTRIX-SERVICE")
+@FeignClient(value = "CLOUD-PROVIDER-HYSTRIX-SERVICE", fallback = OrderHystrixFallbackService.class)
 public interface OrderHystrixService {
     @GetMapping("/payment/hystrix/ok/{id}")
     String paymentInfo_ok(@PathVariable("id") Integer id);
