3aff73f78948017c54615654a4cd1cde49dd25bd
Lrwei <a2413044193@gmail.com>
2023-09-12 16:40:13 +0800

    sentinel 服务熔断+ openfeign

--- SpringFrame/SpringCloud/cloudalibaba-consumer-nacos-order84/src/main/java/com/atguigu/springcloud/controller/CircleBreakerController.java
+++ SpringFrame/SpringCloud/cloudalibaba-consumer-nacos-order84/src/main/java/com/atguigu/springcloud/controller/CircleBreakerController.java
@@ -4,7 +4,9 @@ import com.alibaba.csp.sentinel.annotation.SentinelResource;
 import com.alibaba.csp.sentinel.slots.block.BlockException;
 import com.atguigu.springcloud.entities.CommonResult;
 import com.atguigu.springcloud.entities.Payment;
+import com.atguigu.springcloud.service.PaymentService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -48,5 +50,17 @@ public class CircleBreakerController {
     public CommonResult blockHandler(@PathVariable Long id, BlockException blockException) {
         Payment payment = new Payment(id, "null");
         return new CommonResult(445, "blockHandler-sentinel限流,无此流水: blockException  " + blockException.getMessage(), payment);
+    }
+
+    //==================OpenFeign
+    @Resource
+    private PaymentService paymentService;
+
+    @GetMapping(value = "/consumer/openfeign/{id}")
+    public CommonResult paymentSQL(@PathVariable("id") Long id) {
+        if (id == 4) {
+            throw new RuntimeException("没有该id");
+        }
+        return paymentService.paymentSQL(id);
     }
 }
